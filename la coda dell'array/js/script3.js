69b6ccfdf4ba350486f86d1d5ef1c338aa4ea37c
javoxpl <liacovone7@outlook.it>
2022-05-23 13:26:03 +0200

    consegna e test

---
+++ la coda dell'array/js/script3.js
@@ -0,0 +1,12 @@
+/*Creare un array con un numero a piacere di elementi.
+Stampare a schermo, con un ciclo, gli ultimi 5 elementi dell’array.
+Attenzione #1: quanti elementi minimo dovrà contenere l’array?
+Attenzione #2: il ciclo deve essere costruito per estrarre gli ultimi elementi da qualsiasi array, anche se non conosciamo a priori quanti elementi contiene.
+Extra Bonus
+Creiamo un array chiedendo all’utente quanti elementi dovrà contenere e generando tanti numeri interi casuali quanti sono gli elementi da inserire.
+Attenzione #3: quali controlli dobbiamo fare sull’input dell’utente?
+Super Extra Bonus: chiedere all’utente anche quanti elementi vanno estratti dal fondo dell’array.*/
+
+const test = "hello world";
+
+console.log(test);
---
+++ mail/js/script1.js
@@ -0,0 +1,8 @@
+/*Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+(non usare includes() o indexOf)*/
+
+const test = "hello world";
+
+console.log(test);
