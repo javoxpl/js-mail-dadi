960f47a7ed6bab098ddaf946e1388f3492b649a5
javoxpl <liacovone7@outlook.it>
2022-05-23 16:27:55 +0200

    programma mail funzionante (console)

--- mail/js/script1.js
+++ mail/js/script1.js
@@ -3,6 +3,26 @@ controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 (non usare includes() o indexOf)*/
 
+/*test
 const test = "hello world";
 
 console.log(test);
+*/
+
+
+function findMail(mail, mailGiuste) {
+    let mailFound = "mail errata";
+    for (let i = 0; i < mailGiuste.length; i++) {
+        if (mailGiuste[i] === mail) {
+            mailFound = "mail corretta";
+        }
+    }
+    console.log (mailFound);
+}
+
+let mail = prompt("inserisci la mail");
+const mailConsentite = ["lorenzo.iacovone@gmail.com", "iacovone.lorenzo@outlook.it", "liacovone@hotmail.it"];
+findMail(mail, mailConsentite);
+
+
+
